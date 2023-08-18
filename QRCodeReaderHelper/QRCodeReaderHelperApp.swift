ebfcca28caa567c63f100fd38dce35156ca473c4
Adrien SURUGUE <adrien.surugue@gmail.com>
2023-08-18 15:46:35 +0200

    Initial Commit

---
+++ QRCodeReaderHelper/QRCodeReaderHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeReaderHelperApp.swift
+//  QRCodeReaderHelper
+//
+//  Created by Adrien Surugue on 18/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeReaderHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
